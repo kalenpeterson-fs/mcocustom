7362335d0c193971ee46614aedbb226bd705b67d
Kalen Peterson <kpeterson@forsythe.com>
2016-08-10 08:48:11 -0500

    Fix activate_when logic

--- files/mcollective/agent/deploy.rb
+++ files/mcollective/agent/deploy.rb
@@ -1,7 +1,15 @@
 module MCollective
   module Agent
     class Deploy<RPC::Agent
-      activate_when { Facts["app_6000"] }
+
+      if Facts["osfamily"] == 'windows'
+        $app_6000_path = 'C:\Program Files\App_6000\Code'
+      else
+        $app_6000_path = '/opt/app_6000/code_base'
+      end
+      activate_when do
+        File.directory?($app_6000_path)
+      end
 
       action "deploycode" do
         validate :hostname, String
@@ -20,11 +28,11 @@ module MCollective
 
         reply[:result] = "Deployed #{codebase} v#{codeversion} from #{hostname} to #{localcodedir}"
 
-        #reply[:status] = run(
-        #  "builder_5000_client #{hostname} #{codebase} #{codeversion} #{localcodedir}",
-        #  :stdout => :out,
-        #  :stderr => :err
-        #)
+        reply[:status] = run(
+          "builder_5000_client #{hostname} #{codebase} #{codeversion} #{localcodedir}",
+          :stdout => :out,
+          :stderr => :err
+        )
       end
     end
   end
